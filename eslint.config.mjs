9bb1a545e14112753b0d2e4c69a9bd71343a9910
Sergio Piniella <piniella.sergio@yandex.ru>
2025-01-08 12:47:06 +0300

    Setting up ESLint and checking the code

---
+++ eslint.config.mjs
@@ -0,0 +1,23 @@
+import globals from "globals";
+import pluginJs from "@eslint/js";
+
+/** @type {import('eslint').Linter.Config[]} */
+
+export default {
+  env: {
+    browser: true,
+    es2021: true,
+  },
+
+  extends: "eslint:recommended",
+  parserOptions: {
+    ecmaVersion: 12,
+    sourceType: "module",
+  },
+
+  rules: {
+    indent: ["error", 2],
+    quotes: ["error", "single"],
+    semi: ["error", "always"],
+  },
+};
